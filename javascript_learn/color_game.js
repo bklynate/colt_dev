690ce27f2ebfae522db44ee640fabd83cf41df0c
bklynate <nathanielcfa@gmail.com>
2016-04-19 19:53:53 -0400

    add fleshing out game logic and mechanics for picking the right color

--- javascript_learn/color_game.js
+++ javascript_learn/color_game.js
@@ -1,17 +1,9 @@
-// list of colors
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(134, 0, 255)",
-    "rgb(222, 34, 189)",
-    "rgb(135, 156, 90)",
-    "rgb(225, 156, 110)"
-]
+var colors = generateSquareColors(6)
 // selects all squares
 var squares = document.querySelectorAll(".square");
 
 // hard coded a picked color for development persons
-var pickedColor = colors[3];
+var pickedColor = pickedColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 
 // the random rgb color presented in the h1
@@ -24,11 +16,47 @@ for(var i = 0; i < squares.length; i++){
   //add click listeners to all squares
   squares[i].addEventListener("click", function(){
     var clickedSquare = this.style.background;
-
     if(clickedSquare == pickedColor){
-      alert("You clicked the RIGHT square");
+      prompt("WINNER!!!");
+      changeColors(pickedColor)
     } else {
       alert("You clicked the WRONG square");
+      this.style.background = "#232323";
     }
   });
+}
+
+function randomColor() {
+  // generate a random int between 0 and 255
+  // r - red | b - blue | g - green
+  var r = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+
+  return "rgb("+r+", "+b+", "+g+")";
+}
+
+function generateSquareColors(num){
+  // empty list of colors
+  var colors = [];
+
+  // creates how ever many colors of 'num'
+  for(var n = 0; n < num; n++){
+    colors.push(randomColor());
+  }
+  // returns a complete list of random colors
+  return colors;
+}
+
+function pickedColor(){
+  // this gets a random number between 0 and the length of
+  // the colors array
+  var randoNum = Math.floor(Math.random() * colors.length)
+  return colors[randoNum]
+}
+
+function changeColors(color){
+  for(var n = 0; n < squares.length; n++){
+    squares[n].style.background = color;
+  }
 }
