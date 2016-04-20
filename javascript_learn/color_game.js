af0cb4531399c736da11af6322c8aebdd3f7e55d
bklynate <nathanielcfa@gmail.com>
2016-04-20 16:49:03 -0400

    edit code structure and missed semicolons

--- javascript_learn/color_game.js
+++ javascript_learn/color_game.js
@@ -17,7 +17,7 @@ function pickedColor(){
   // this gets a random number between 0 and the length of
   // the colors array
   var randoNum = Math.floor(Math.random() * colors.length)
-  return colors[randoNum]
+  return colors[randoNum];
 }
 
 function changeColors(color){
@@ -26,11 +26,22 @@ function changeColors(color){
   }
 }
 
+function randomColor() {
+  // generate a random int between 0 and 255
+  // r - red | b - blue | g - green
+  var r = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+
+  return "rgb("+r+", "+b+", "+g+")";
+}
 /*
           GLOBAL VARIABLES
 */
 
-var colors = generateSquareColors(6)
+var colors = generateSquareColors(6);
+
+var resetButton = document.querySelector("#resetButton")
 // selects all squares
 var squares = document.querySelectorAll(".square");
 
@@ -39,7 +50,7 @@ var pickedColor = pickedColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 
 // game messaging area
-var messageArea = document.querySelector("#message")
+var messageArea = document.querySelector("#message");
 
 // the random rgb color presented in the h1
 // is the color of the square you must pick
@@ -67,37 +78,7 @@ for(var i = 0; i < squares.length; i++){
   });
 }
 
-function randomColor() {
-  // generate a random int between 0 and 255
-  // r - red | b - blue | g - green
-  var r = Math.floor(Math.random() * 256);
-  var b = Math.floor(Math.random() * 256);
-  var g = Math.floor(Math.random() * 256);
+resetButton.addEventListener("click", function(){
+  console.log("Who clicked me?")
+});
 
-  return "rgb("+r+", "+b+", "+g+")";
-}
-
-function generateSquareColors(num){
-  // empty list of colors
-  var colors = [];
-
-  // creates how ever many colors of 'num'
-  for(var n = 0; n < num; n++){
-    colors.push(randomColor());
-  }
-  // returns a complete list of random colors
-  return colors;
-}
-
-function pickedColor(){
-  // this gets a random number between 0 and the length of
-  // the colors array
-  var randoNum = Math.floor(Math.random() * colors.length)
-  return colors[randoNum]
-}
-
-function changeColors(color){
-  for(var n = 0; n < squares.length; n++){
-    squares[n].style.background = color;
-  }
-}
